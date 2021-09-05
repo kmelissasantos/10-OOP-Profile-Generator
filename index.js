a69b7103cb5947d517fd78f93c359e06e85c110e
Melissa Santos <melissasantos@Melissas-MacBook-Pro.local>
2021-09-05 15:08:36 -0400

    updated index.js

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-const managerQuestions 
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Please provide your name",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Please provide your ID"
+    }
+]
