eae3b640a3c1367370aa1b57139b9b0d448be096
Melissa Santos <melissasantos@Melissas-MacBook-Pro.local>
2021-09-05 15:27:07 -0400

    added validation to index.js

--- index.js
+++ index.js
@@ -9,21 +9,45 @@ const managerQuestions = [
         type: 'input',
         name: 'name',
         message: "Please provide your name",
+        validate: (input) => {
+            if (input === '') {
+                return "Please make sure you provide your name here."
+            }
+            return true;
+        }
     },
     {
         type: 'input',
         name: 'id',
-        message: "Please provide your ID"
+        message: "Please provide your ID",
+        validate: (input) => {
+            if (input === '') {
+                return "Please make sure you provide your ID here."
+            }
+            return true;
+        }
     },
     {
         type: 'input',
         name: 'email',
-        message: "Please provide your email?"
+        message: "Please provide your email?",
+        validate: (input) => {
+            if (input === '') {
+                return "Please make sure you provide your email here."
+            }
+            return true;
+        }
     },
     {
         type: 'input',
         name: 'officeNumber',
-        message: "What is your office number?"
+        message: "What is your office number?",
+        validate: (input) => {
+            if (input === '') {
+                return "Please make sure you provide your office number here."
+            }
+            return true;
+        }
     },
     {
         type: 'confirm',
